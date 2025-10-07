e427e3b151a87257d6bf55734b7913590b5fe96a
swanandkeskar <swanandsk02@gmail.com>
2025-10-07 19:22:02 +0530

    corrected user creation

--- server/controllers/webHooks.js
+++ server/controllers/webHooks.js
@@ -24,7 +24,7 @@ try {
         }
         case 'user.updated':{
             const userData={
-                email:data.email_address[0].email_address,
+                email:data.email_addresses[0].email_address,
                 name:data.first_name+" "+data.last_name,
                 imageUrl:data.image_url,
             }
